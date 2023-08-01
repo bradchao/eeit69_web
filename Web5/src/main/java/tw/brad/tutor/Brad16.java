76e81c3b1ef5c228cf6956439ec0f82595e87068
User <User@R1802.r1802.tciii.org.tw>
2023-08-01 15:26:50 +0800

    08011526

--- Web5/src/main/java/tw/brad/tutor/Brad16.java
+++ Web5/src/main/java/tw/brad/tutor/Brad16.java
@@ -10,26 +10,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import tw.brad.apis.Bike;
-
-@WebServlet("/Brad14")
-public class Brad14 extends HttpServlet {
+@WebServlet("/Brad16")
+public class Brad16 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
-		RequestDispatcher dis = request.getRequestDispatcher("Brad15");
-		
-		Bike b1 = new Bike("brad");
-		request.setAttribute("b1", b1);
-		b1.upSpeed();b1.upSpeed();b1.upSpeed();b1.upSpeed();
-		
-		
+		RequestDispatcher dis = request.getRequestDispatcher("Brad17");
+	
 		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = response.getWriter();
-		out.println("<h1>Brad Company</h1>");
+		out.println("<h1>Brad Company16</h1>");
 		out.println("<hr />");
-		dis.include(request, response);
+		dis.forward(request, response);
 		out.println("<hr />");
 		out.println("Copyleft...");
+		
 	}
 
 }
--- Web5/src/main/java/tw/brad/tutor/Brad17.java
+++ Web5/src/main/java/tw/brad/tutor/Brad17.java
@@ -9,27 +9,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import tw.brad.apis.Bike;
-
-@WebServlet("/Brad15")
-public class Brad15 extends HttpServlet {
+@WebServlet("/Brad17")
+public class Brad17 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
-		int max = 0;
-		try {
-			max = Integer.parseInt(request.getParameter("max"));
-		}catch(Exception e) {}
-		
 		PrintWriter out = response.getWriter();
-		out.print((int)(Math.random()*max+1));
-		out.println("<hr />");
-		
-		Object obj = request.getAttribute("b1");
-		Bike b1 = (Bike)obj;
-		
-		out.print(b1);
-		
-	
+		out.println("<h1>Brad Company17</h1>");
 	}
 
 }
